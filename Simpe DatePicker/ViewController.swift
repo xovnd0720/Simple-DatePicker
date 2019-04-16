f0df073dbf33fb792f98331ec48894ab0096a144
Jonghyun Kim <jhkim@dit.ac.kr>
2019-04-16 11:18:42 +0900

    Initial Commit

---
+++ Simpe DatePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simpe DatePicker
+//
+//  Created by amadeus on 16/04/2019.
+//  Copyright © 2019 DIT Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
